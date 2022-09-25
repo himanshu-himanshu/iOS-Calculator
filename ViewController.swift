190a36ec9379fa154bede98f2cab329225ef83d4
Himanshu <himanshu16315@gmail.com>
2022-09-25 16:33:25 -0400

    Connections made in ViewController file to all buttons and label

--- ViewController.swift
+++ ViewController.swift
@@ -1,19 +1,96 @@
-//
-//  ViewController.swift
-//  Assignment 1 Calculator UI
-//
-//  Created by Himanshu on 2022-09-25.
-//
+//  Authors: Himanshu (301296001) & Gurminder
+//  Group: 1
+//  Subject: MAPD714
+//  Assignment: 1
+//  Task: Create Calculator App UI
+//  File Name: ViewController.swift
+//  About App: Simple calculator app with several funtionalities like add, subtract, multiply, divide.
+//  Last modified: 25/09/2022
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Result label connection
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    /** Numeric Buttons Connection */
+    
+    @IBAction func sevenTap(_ sender: Any) {
+    }
+    
+    @IBAction func eightTap(_ sender: Any) {
+    }
 
-
+    @IBAction func nineTap(_ sender: Any) {
+    }
+    
+    @IBAction func fourTap(_ sender: Any) {
+    }
+    
+    @IBAction func fiveTap(_ sender: Any) {
+    }
+    
+    @IBAction func sixTap(_ sender: Any) {
+    }
+    
+    @IBAction func oneTap(_ sender: Any) {
+    }
+    
+    @IBAction func twoTap(_ sender: Any) {
+    }
+    
+    @IBAction func threeTap(_ sender: Any) {
+    }
+    
+    @IBAction func decimalTap(_ sender: Any) {
+    }
+    
+    @IBAction func zeroTap(_ sender: Any) {
+    }
+    
+    /** Clear Button Connection */
+    
+    @IBAction func clearBtn(_ sender: Any) {
+    }
+    
+    /** Backspace Button Connection */
+    
+    @IBAction func backspaceBtn(_ sender: Any) {
+    }
+    
+    /** Operator Buttons Connection */
+    
+    @IBAction func plusMinusBtn(_ sender: Any) {
+    }
+    
+    @IBAction func modulusBtn(_ sender: Any) {
+    }
+    
+    @IBAction func addBtn(_ sender: Any) {
+    }
+    
+    @IBAction func subtractBtn(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyBtn(_ sender: Any) {
+    }
+    
+    @IBAction func divideBtn(_ sender: Any) {
+    }
+    
+    /** Result Button Connection */
+    
+    @IBAction func resultBtn(_ sender: Any) {
+    }
+    
+    
 }
 
